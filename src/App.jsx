22a1f95c6a2b70a9bea8f16b6713031e527ab309
JustANipple <prandisamuel99@gmail.com>
2023-09-12 17:58:52 +0200

    modified:   src/App.css 	modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,34 @@ import Home from './components/Home/Home';
 import data from "../public/data.json"
 import Destination from './components/Destination/Destination';
 
+
+
 function App() {
+
+  const backgrounds = [
+    {
+      name: "home",
+      bgs: ["background-home-mobile.jpg", "background-home-tablet.jpg", "background-home-desktop.jpg"]
+    },
+    {
+      name: "destination",
+      bgs: ["background-destination-mobile.jpg", "background-destination-tablet.jpg", "background-destination-desktop.jpg"]
+    },
+    {
+      name: "crew",
+      bgs: ["background-crew-mobile.jpg", "background-crew-tablet.jpg", "background-crew-desktop.jpg"]
+    },
+    {
+      name: "technology",
+      bgs: ["background-crew-mobile.jpg", "background-crew-tablet.jpg", "background-crew-desktop.jpg"]
+    },
+  ]
 
   const [menu, setMenu] = useState(false);
   const [option, setOption] = useState(0);
+  const [background, setBackground] = useState()
+  
+  document.body.style.backgroundImage = "url(src/assets/home/background-home-mobile.jpg)";
 
   return (
     <>
