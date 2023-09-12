7fc9de4f87b67e2c2380f34d011ef2d90243bf99
JustANipple <prandisamuel99@gmail.com>
2023-09-12 12:13:40 +0200

    modified:   src/components/Header/Header.module.css 	modified:   src/components/Home/Home.jsx 	modified:   src/components/Home/Home.module.css 	modified:   src/components/Nav/Nav.module.css

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
             <p className={styles.appendix}>so, you want to travel to</p>
             <h1 className={styles.title}>space</h1>
             <p className={styles.paragraph}>
-                let's face it, if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we'll give you a truly out of this world experience!
+                Let's face it, if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we'll give you a truly out of this world experience!
             </p>
         </div>
         <button className={styles.explore_button}>
